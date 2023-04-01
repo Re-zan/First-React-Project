eaa29e8e2161846f829b86ea014e531ff5fc2109
Re-zan <zannat881all@gmail.com>
2023-04-01 09:46:02 +0600

    First component made

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
-      <h3>iugisdgio</h3>
+      <Header></Header>
     </div>
   );
 }
