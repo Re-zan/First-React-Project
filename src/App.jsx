899f379d51d3a088f536a51b323b3a77310ef9bc
Re-zan <zannat881all@gmail.com>
2023-04-01 10:48:33 +0600

    fake data making completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Mainbody from "./components/Mainbody/Mainbody";
 
 function App() {
   return (
     <div className="container">
       <Header></Header>
+      <Mainbody></Mainbody>
     </div>
   );
 }
---
+++ src/components/Mainbody/Mainbody.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Mainbody = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm-6 col-md-8">Column1</div>
+        <div className="col-sm-6 col-md-4">Column2</div>
+      </div>
+    </div>
+  );
+};
+
+export default Mainbody;
